b56f1e977d15335b4fefa917ed76a0b4fb374bf0
Dhrubo Paul <dhrubojpaul@gmail.com>
2022-09-05 22:12:17 -0400

    com.sun.net.httpserver included

---
+++ app/src/main/java/chatmessenger/http/ChatHttpHandler.java
@@ -0,0 +1,15 @@
+package chatmessenger.http;
+
+import java.io.IOException;
+
+import com.sun.net.httpserver.*;
+
+public class ChatHttpHandler implements HttpHandler{
+
+    @Override
+    public void handle(HttpExchange arg0) throws IOException {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
